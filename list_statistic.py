3f643b030e195b636fc3a6bab37fc14ed12c2144
Rosen Ivanov <143969365+RosenCodes20@users.noreply.github.com>
2023-09-27 20:18:16 +0300

    Created list_statistic.py
    Added a new list project.

---
+++ list_statistic.py
@@ -0,0 +1,30 @@
+following_lines = int(input())
+positive_list = []
+
+negative_list = []
+
+sum_of_neg = 0
+
+count_of_pos = 0
+for line in range(following_lines):
+    number = int(input())
+    
+    if number >= 0:
+        count_of_pos += 1
+        
+        positive_list.append(number)
+        
+    else:
+        negative_list.append(number)
+        
+sum_of_neg=sum(negative_list)
+        
+print(positive_list)
+
+print(negative_list)
+
+print(f"Count of positives: {count_of_pos}")
+
+print(f"Sum of negatives: {sum_of_neg}")
+
+
